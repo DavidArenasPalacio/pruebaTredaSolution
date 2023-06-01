4ee062f4f628575575c288ad1f3f7dd4014f570d
David <davidarenasp123@gmail.com>
2023-05-31 19:12:38 -0500

    Frase invertida segun la cantidad de cadena de texto, terminado

--- pruebaDos.php
+++ pruebaDos.php
@@ -1,1 +1,20 @@
 <?php 
+/* 
+    Dada una frase, devolver la frase donde las palabras con mayor a 5 letras estén al
+    revés. Ejemplo: Dado “Bienvenido a Treda Solutions” retornar “odinevneiB a Treda
+    snoitulos
+*/
+
+$frase = "Bienvenido a Treda Solutions";
+$cadenaSeparado = explode(" ", $frase);
+
+$cadenaInvertida = "";
+foreach ($cadenaSeparado as $key => $value) {
+    if(strlen($value) > 5){
+        $cadenaInvertida .= strrev($value) . " ";
+    }else {
+        $cadenaInvertida .= $value . " ";
+    }
+}
+
+echo $cadenaInvertida;
